29a417b1d72f5cca5a056c59d144ecd991d0efbd
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2024-08-28 16:06:46 +0100

    Refactor Aiport

--- lib/airport.rb
+++ lib/airport.rb
@@ -3,6 +3,8 @@ require './lib/plane'
 class Airport
   attr_accessor :apron, :stormy
 
+  STORMY_THRESHOLD = 6
+
   def initialize
     @apron = []
     @stormy = 0
@@ -10,23 +12,17 @@ class Airport
 
   def land_plane(plane)
     if stormy?
-      passage_of_time
-      raise 'It is too stormy to land!' 
+      stormy_error('It is too stormy to land!')
     else
-      apron << plane
-      print 'The plane has landed!'
-      passage_of_time
+      initiate_landing(plane)
     end
   end
 
   def take_off(plane)
     if stormy?
-      passage_of_time
-      raise 'It is too stormy to take off!'
+      stormy_error('It is too stormy to take off!')
     else
-      apron.delete_at(apron.index(plane))
-      print 'The plane has taken off!'
-      passage_of_time
+      initiate_take_off(plane)
     end
   end
 
@@ -37,6 +33,23 @@ class Airport
   end
 
   def stormy?
-    stormy > 6
+    stormy > STORMY_THRESHOLD
+  end
+
+  def stormy_error(error_message)
+    passage_of_time
+    raise error_message
+  end
+
+  def initiate_landing(plane)
+    apron << plane
+    print 'The plane has landed!'
+    passage_of_time
+  end
+
+  def initiate_take_off(plane)
+    apron.delete_at(apron.index(plane))
+    print 'The plane has taken off!'
+    passage_of_time
   end
 end
