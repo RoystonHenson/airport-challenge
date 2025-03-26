19bcf4d31537d01643143b5b02b35c2fa5646263
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2025-03-26 18:27:30 +0000

    Add feature: Plane can take off from airport

--- lib/plane.rb
+++ lib/plane.rb
@@ -3,4 +3,8 @@ class Plane
   def land(airport)
     "The plane has landed at #{airport} airport!"
   end
+
+  def take_off(airport)
+    "The plane has left #{airport} airport!"
+  end
 end
--- spec/plane_spec.rb
+++ spec/plane_spec.rb
@@ -9,4 +9,10 @@ describe Plane do
       expect(plane.land(airport)).to eq("The plane has landed at #{airport} airport!")
     end
   end
+
+  describe '#take_off' do
+    it 'takes off' do
+      expect(plane.take_off(airport)).to eq("The plane has left #{airport} airport!")
+    end
+  end
 end
