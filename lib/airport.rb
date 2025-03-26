0ed4245a1502efa84ed80c848796d85327a15412
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2025-03-26 20:06:29 +0000

    Rewrite .airport

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,65 +1,15 @@
 require './lib/plane'
 
 class Airport
-  attr_reader :capacity
-  attr_accessor :apron, :stormy
+    attr_reader :apron, :capacity
 
-  STORMY_THRESHOLD = 6
-  CAPACITY = 5
-
-  def initialize(capacity=CAPACITY)
+  def initialize
     @apron = []
-    @stormy = 0
-    @capacity = capacity
+    @capacity = 5
   end
 
-  def land_plane(plane)
-    check_if_landed(plane)
-    check_if_full
-    stormy? ? stormy_error('It is too stormy to land! Try again later.') : initiate_landing(plane) 
-  end
-
-  def take_off(plane)
-    check_if_in_airport(plane)
-    stormy? ? stormy_error('It is too stormy to take off! Try again later.') : initiate_take_off(plane)
-  end    
-
-  private
-
-  def passage_of_time
-    @stormy = rand(11)
-  end
-
-  def stormy?
-    stormy > STORMY_THRESHOLD
-  end
-
-  def stormy_error(error_message)
-    passage_of_time
-    raise error_message
-  end
-
-  def initiate_landing(plane) #change to aircraft to acoomodate other flying vehicles?
+  def allow_arrival(plane)
+    plane.land
     apron << plane
-    print 'The plane has landed!'
-    passage_of_time
-  end
-
-  def initiate_take_off(plane)
-    apron.delete_at(apron.index(plane))
-    print 'The plane has taken off!'
-    passage_of_time
-  end
-
-  def check_if_full
-    raise 'The airport is full!' if @apron.size >= capacity
-  end
-
-  def check_if_landed(plane)
-    raise 'That plane has already landed!' if apron.include?(plane)
-  end
-
-  def check_if_in_airport(plane)
-    raise 'That plane is not in this airport!' if !apron.include?(plane)
   end
 end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -2,101 +2,26 @@ require 'airport'
 
 describe Airport do
   let(:airport) { Airport.new }
-  let(:plane)   { double('plane') }
-  let(:plane2)  { double('plane2')}
+  let(:plane) { double('plane') }
 
-  before(:all) do
-    @original_stdout = $stdout
-    $stdout = File.open(File::NULL, 'w')
+  before(:each) do
+    allow(plane).to receive(:land)
   end
 
-  after(:all) do
-    $stdout.close
-    $stdout = @original_stdout
-  end
-    
   describe '#initialize' do
-    it 'has an apron to park planes' do
+    it 'has an apron' do
       expect(airport.apron).to eq([])
     end
-  
-    context 'when not given an argument' do
-      it 'has a default capacity of 5' do
-        expect(airport.capacity).to eq(5)
-      end
-    end
 
-    context 'when given an argument' do
-      it 'can land planes equal to the capacity set by user' do
-        airport = Airport.new(1)
-        airport.land_plane(plane)
-        expect { airport.land_plane(plane2)}.to raise_error(RuntimeError, 'The airport is full!') 
-      end
-    end
-  end
-  
-  describe '#land_plane' do
-    context 'when weather is good and there is space in the airport' do
-      it 'raises error for plane already landed' do
-        airport.land_plane(plane)
-        airport.stormy = 0
-        expect { airport.land_plane(plane) }.to raise_error(RuntimeError, 'That plane has already landed!')
-      end
-
-      it 'lands a plane' do
-        airport.land_plane(plane)
-        expect(airport.apron).to eq([plane])
-      end
-  
-      it 'outputs a landing message' do
-        expect { airport.land_plane(plane) }.to output('The plane has landed!').to_stdout 
-      end
-    end
-
-    context 'when weather is good but the airport is full' do
-      it 'raises error for airport full' do
-        5.times { airport.apron << double('plane') }
-        expect { airport.land_plane(plane) }.to raise_error(RuntimeError, 'The airport is full!')
-        expect(airport.apron.size).to eq(5)
-      end
-    end
-
-    context 'when weather is stormy' do
-      it 'raises error preventing landing' do
-        airport.stormy = 10
-        expect { airport.land_plane(plane) }.to raise_error('It is too stormy to land! Try again later.')
-      end
+    it 'has a capacity of 5' do
+      expect(airport.capacity).to eq(5)
     end
   end
 
-  describe '#take_off' do
-    before(:each) do
-      airport.apron = [plane]
-    end
-
-    context 'when weather is good and plane is in the airport' do
-      it 'lets a plane take off' do
-        airport.take_off(plane)
-        expect(airport.apron).to eq([])
-      end
-
-      it 'outputs a take-off message' do
-        expect { airport.take_off(plane) }.to output('The plane has taken off!').to_stdout
-      end
-    end
-
-    context 'when weather is good but plane is not in the airport' do
-      it 'raises error for plane not in airport' do
-        airport.take_off(plane)
-        expect { airport.take_off(plane) }.to raise_error(RuntimeError, 'That plane is not in this airport!')
-      end
-    end
-
-    context 'when weather is stormy' do
-      it 'raises error preventing take-off' do
-        airport.stormy = 10
-        expect { airport.take_off(plane) }.to raise_error(RuntimeError, 'It is too stormy to take off! Try again later.')
-      end
+  describe '#allow_arrival' do
+    it 'accepts the flight arrival' do
+      airport.allow_arrival(plane)
+      expect(airport.apron).to eq([plane])
     end
   end
 end
