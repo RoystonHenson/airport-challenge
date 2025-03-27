3e69072b20e3548a0f8cd1b9cb13d86a60b9e5d4
Royston Henson <19607023+RoystonHenson@users.noreply.github.com>
2025-03-27 18:17:02 +0000

    Add edge case: Planes cannot land when already landed

--- lib/airport.rb
+++ lib/airport.rb
@@ -1,14 +1,14 @@
 require './lib/plane'
 
 class Airport
-    attr_reader :apron, :capacity
+  attr_reader :apron, :capacity
 
   def initialize
     @apron = []
     @capacity = 5
   end
 
-  def allow_arrival(plane)
+  def permit_landing(plane)
     plane.land
     apron << plane
   end
--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,8 +1,9 @@
 require 'airport'
 
 describe Airport do
-  let(:airport) { Airport.new }
-  let(:plane) { double('plane') }
+  let(:airport)  { Airport.new }
+  let(:airport2) { Airport.new }
+  let(:plane)    { double('plane') }
 
   before(:each) do
     allow(plane).to receive(:land)
@@ -18,10 +19,25 @@ describe Airport do
     end
   end
 
-  describe '#allow_arrival' do
+  describe '#permit_landing' do
     it 'accepts the flight arrival' do
-      airport.allow_arrival(plane)
+      airport.permit_landing(plane)
       expect(airport.apron).to eq([plane])
+    end
+  end
+
+  describe '#landing_check' do
+    context 'when a plane has already landed' do
+      #allow(:plane).to receive(:airborne)and_return(false)
+      it 'raises an error when trying to land again' do
+        expect { airport.permit_landing(plane) }.to raise_error(RuntimeError, 'That plane is currently on the ground!')
+      end
+    end
+    context 'when a landed plane tries to land at another airport' do
+
+      it 'raises an error that it is on the ground' do
+        expect { airport2.permit_landing(plane) }.to raise_error(RuntimeError, 'That plane is currently on the ground!')
+      end
     end
   end
 end
